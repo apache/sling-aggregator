733d9bd483462fcd994ef2bc729bcae82e29ec69
Konrad Windszus <kwin@apache.org>
2021-09-22 09:29:23 +0200

    add revision per project

--- collect-sling-repos.groovy
+++ collect-sling-repos.groovy
@@ -95,9 +95,9 @@ xml.manifest() {
     slingRepos.forEach { repo ->
         def groups = getGroups(repo, groupMap)
         if ( groups ) {
-            project(path: repoPath(repo), name: "${repo.name}.git", description: "${repo.description}", groups: getGroups(repo, groupMap))
+            project(path: repoPath(repo), name: "${repo.name}.git", description: "${repo.description}", groups: getGroups(repo, groupMap), revision: "${repo.default_branch}")
         } else {
-            project(path: repoPath(repo), name: "${repo.name}.git", description: "${repo.description}")
+            project(path: repoPath(repo), name: "${repo.name}.git", description: "${repo.description}", revision: "${repo.default_branch}")
         }
     }
 }
